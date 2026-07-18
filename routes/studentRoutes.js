b1757fd47303fcafdc4579416bd65897f549fd7b
Zahid hasan <zmzahidhasan181@gmail.com>
2026-07-18 16:33:20 +0600

    update

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,23 +1,23 @@
 // import express from "express";
-// import studentController from "../controllers/studentController";
-// import { protect, authorize } from "../middlewares/authMiddleware";
-// import ROLES from "../constants/roles";
+// import {
+//   addFavoriteTeacher,
+//   getDashboard,
+//   getProfile,
+//   removeFavoriteTeacher,
+//   updateProfile,
+// } from "../controllers/studentController.js";
+// import { protect, authorize } from "../middlewares/authMiddleware.js";
+// // import ROLES from "../constants/roles.js";
 
 // const router = express.Router();
 
-// router.use(protect, authorize(ROLES.STUDENT));
+// router.use(protect, authorize("student"));
 
-// router.get("/dashboard", studentController.getDashboard);
-// router.get("/profile", studentController.getProfile);
-// router.patch("/profile", studentController.updateProfile);
-// router.patch(
-//   "/favorite-teachers/:teacherId",
-//   studentController.addFavoriteTeacher,
-// );
-// router.delete(
-//   "/favorite-teachers/:teacherId",
-//   studentController.removeFavoriteTeacher,
-// );
+// router.get("/dashboard", getDashboard);
+// router.get("/profile", getProfile);
+// router.patch("/profile", updateProfile);
+// router.patch("/favorite-teachers/:teacherId", addFavoriteTeacher);
+// router.delete("/favorite-teachers/:teacherId", removeFavoriteTeacher);
 
 // export default router;
 
@@ -29,8 +29,7 @@ import {
   removeFavoriteTeacher,
   updateProfile,
 } from "../controllers/studentController.js";
-import { protect, authorize } from "../middlewares/authMiddleware.js";
-// import ROLES from "../constants/roles.js";
+import { authorize, protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
